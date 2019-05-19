eace6704dff2b2c04067c4c1467ca7882e839853
greegorov <greegorov0@gmail.com>
2019-05-19 18:17:59 +0300

    Add files via upload

--- immulation of colonies.R
+++ immulation of colonies.R
@@ -1,15 +1,19 @@
-#to calculate probability of monoclonality 
+#To calculate probability of monoclonality perform replicate 10000 times
 ProbOfMonoclonality <- mean(replicate (10000, {
-  N <- 419
-  x <- runif (N, 0, 3.1416*17.5)
-y <- runif (N, 0, 3.1416*17.5)
-r <- rbeta(N, 9.849032,  99.940056)
-C <- cbind(x,y,r)
+  #number of colonies in well
+  N <- 25
+  # (x,y) coordinate of centre. In square with area pi*34.8
+  x <- runif (N, 0, 10)
+  y <- runif (N, 0, 10)
+  #Beta distribution  with alpha = 9.849032, beta = 99.940056
+  r <- rbeta(N, 9.849032,  99.940056)
+  C <- cbind(x,y,r)
 
 #function which check if two random colonies overlap in matrix N x 3
 func.c2 <- function(i,j) {
   (C[i,1]-C[j,1])^2+(C[i,2]-C[j,2])^2 <= (C[i,3]+C[j,3])^2
 }
+#dp - matrix to fill with results of tested colonies
 dp <- matrix(ncol = N, nrow = N)
   for (i in c(1:N)) {
   dp[i,] <- func.c2(i, )
