4f3d29d9120fafe8cec363fb4855a2b78487854f
vasily-pavelko <greegorov0@gmail.com>
2025-09-03 18:41:28 -0400

    Update repository structure and plot generation script
    - Updated README.md with article information and authors
    - Reorganized repository with data/ and results/ folders
    - Updated R script to generate 4 publication-ready figures
    - Renamed Excel file references to clonepix_data.xlsx
    - Removed letter labels from plots as requested
    - Added proper usage instructions and documentation

--- README.md
+++ README.md
@@ -1,6 +1,42 @@
-# calculating-probability-of-circle-overlaping-by-Monte-Carlo-similation
+<<<<<<< HEAD
+# Monte Carlo Method for Calculating the Probability of Monoclonality of Cell Lines
+
+## Plot Generation Script for Article
+
+**Authors:** S. S. Timonovaa, *, V. I. Pavelko, I. A. Kirica, V. N. Badea, A. A. Piskunova, and R. A. Khamitova
+
+## Overview
+
+This repository contains the R script for generating figures used in the article "Monte Carlo Method for Calculating the Probability of Monoclonality of Cell Lines". The script implements a Monte Carlo simulation to calculate the probability of monoclonality by generating N colonies with centers at (x,y) coordinates and radius r.
+
+The radius distribution of colonies is determined from ClonePix FL statistics. The simulation generates N colonies and tests each colony against all others for overlap. The diagonal elements (self-overlapping) are excluded from the analysis. The sum of the overlap matrix represents the number of overlapped colonies, which when normalized by the total number of colonies gives the probability of monoclonality.
+
+## Generated Figures
+
+Fig. 2. Calculation of the probability of monoclonality of cell lines.
+(a) Histogram of radius values of grown colonies;
+(b) selection of distribution according to the Cullen and Frey plot;
+(c) comparison of the selected distribution and real values of colony radii;
+(d) an example of a well with simulated colonies.
+
+## Usage
+
+Run the main script to generate all figures:
+
+```r
+Rscript "immulation of colonies.R"
+```
+
+The script will generate four figures (Fig2A.png, Fig2B.png, Fig2C.png, Fig2D.png) in the `results/` folder.
+
+## Data
+
+The script uses ClonePix data from `data/clonepix_data.xlsx` containing colony measurements and statistics.
+=======
+# calculating-probability-of-circle-overlapping-by-Monte-Carlo-simulation
 #
 #To calculate probability of monoclonality we generate N colonies with center in (x,y) and raduis r. 
 Radius of colonies has a distribution determined from ClonePix FL statistics.
 Generate N colonies and each of them with each of rest. In diagonal line false poitive overlapping needs to be discounted.
 Sum of matrix equal the number of overlaped colonies. Normed on number colonies is a  probability.
+>>>>>>> refs/remotes/origin/master
--- immulation of colonies.R
+++ immulation of colonies.R
@@ -5,21 +5,21 @@ library(ggplot2)
 library(XML)
 
 #with for loop
-sheet_list <- excel_sheets("ererer.xlsx")
-all_sheets <- data.frame(matrix(ncol = ncol(read_xlsx("ererer.xlsx", sheet = "Auto637678165240625000")), nrow = 0))
-colnames(all_sheets) <- colnames(read_xlsx("ererer.xlsx", sheet = "Auto637678165240625000"))
+sheet_list <- excel_sheets("data/clonepix_data.xlsx")
+all_sheets <- data.frame(matrix(ncol = ncol(read_xlsx("data/clonepix_data.xlsx", sheet = "Auto637678165240625000")), nrow = 0))
+colnames(all_sheets) <- colnames(read_xlsx("data/clonepix_data.xlsx", sheet = "Auto637678165240625000"))
 for (i in sheet_list) {
-  all_sheets <- rbind(all_sheets, read_xlsx("ererer.xlsx", sheet = i))
+  all_sheets <- rbind(all_sheets, read_xlsx("data/clonepix_data.xlsx", sheet = i))
 }
 #show unique for every column
 uniq <- apply(all_sheets, 2, unique)
 str(uniq)
 #with apply function
-sheet_list <- excel_sheets("ererer.xlsx")
+sheet_list <- excel_sheets("data/clonepix_data.xlsx")
 all_sheets_list <- list()
 for (i in sheet_list) {
   all_sheets_list$i <- i
-  all_sheets_list[[i]] <-read_xlsx("ererer.xlsx", sheet = i)
+  all_sheets_list[[i]] <-read_xlsx("data/clonepix_data.xlsx", sheet = i)
 }
 big_data = do.call(rbind, all_sheets_list)
 
@@ -29,19 +29,23 @@ dim(accept)
 summary(accept$RadiusAverageRMS)
 RMS <- as.numeric(gsub(",", ".", accept$RadiusAverageRMS))
 summary(RMS)
-hist(RMS, breaks = 100)
-par(new=TRUE)
+png('results/Fig2A.png')
+hist(RMS, breaks = 100, main = "", 
+     xlab = "Average colony radius, mm", ylab = "Frequency of occurrence")
+dev.off()
+par(new=FALSE)
 
 
 
 
 #model for picked group
-
-descdist(accept$RadiusAverageRMS,  boot = 1000)
+png('results/Fig2B.png')
+descdist(accept$RadiusAverageRMS, boot = 1000)
+dev.off()
 #data according to the  cullen and frey graph is lognormal distributed
 fit_lognorm <- fitdist(accept$RadiusAverageRMS, "lnorm")
 
-plot(fit_lognorm)
+# Skip diagnostic plot for now
 fit_lognorm
 #estimate  Std. Error
 #meanlog -1.5052555 0.007266355
@@ -49,11 +53,16 @@ fit_lognorm
 
 #curve(dlnorm(x, meanlog=fit_lognorm[[1]][1], sdlog=fit_lognorm[[1]][2]), from=0, to=0.4)
 
+png('results/Fig2C.png')
 ggplot() +
   geom_histogram(aes(accept$RadiusAverageRMS, y = ..density..), binwidth = .005) +
   stat_function(fun = function(x) dlnorm(x, meanlog=fit_lognorm[[1]][1], sdlog=fit_lognorm[[1]][2]), color = "red",
                 size = 1) +
-  xlab("Batting average")
+  xlab("Average colony radius, mm") +
+  ylab("Frequency of occurrence") +
+  ggtitle("")
+ggsave('results/Fig2C.png')
+dev.off()
 
 
 
@@ -83,33 +92,19 @@ ProbOfMonoclonality <- mean(replicate (10000, {
 }))
 ProbOfMonoclonality
 
-#with plot
-ProbOfMonoclonality <- mean(replicate (10000, {
-  #number of colonies in well
-  N <- 45
-  # (x,y) coordinate of centre. In square with area pi*34.8
-  x <- runif (N, 0, 31)
-  y <- runif (N, 0, 31)
-  #Beta distribution  with alpha = 9.849032, beta = 99.940056
-  r <- rlnorm(N, meanlog = -1.5052555, sdlog = 0.1677568)
-  C <- cbind(x,y,r)
-  #function which check if two random colonies overlap in matrix N x 3
-  func.c2 <- function(i,j) {
-    (C[i,1]-C[j,1])^2+(C[i,2]-C[j,2])^2 <= (C[i,3]+C[j,3])^2
-  }
-  #dp - matrix to fill with results of tested colonies
-  dp <- matrix(ncol = N, nrow = N)
-  for (i in c(1:N)) {
-    dp[i,] <- func.c2(i, )
-  }
-  sum((colSums(dp)-1) == 0)/N
-  plot(1:31,type="n", asp = 1)
-  index <- c(1:N)
-  for (i in index) {
-    draw.circle(x[i], y[i], r[i])
-  }
+#with plot - single iteration for visualization
+png('results/Fig2D.png')
+N <- 45
+x <- runif(N, 0, 31)
+y <- runif(N, 0, 31)
+r <- rlnorm(N, meanlog = -1.5052555, sdlog = 0.1677568)
+plot(1:31, type="n", asp = 1, xlim = c(0, 31), ylim = c(0, 31),
+     main = "", xlab = "Horizontal coordinates of colony, mm", ylab = "Vertical coordinates of colony, mm")
+index <- c(1:N)
+for (i in index) {
+  draw.circle(x[i], y[i], r[i])
 }
-))
+dev.off()
 
 
 
@@ -123,24 +118,19 @@ all_sheets_gar <- subset(all_sheets, all_sheets$AxisRatio > 0.8)
 all_sheets_gar1 <- subset(all_sheets_gar, all_sheets_gar$Compactness > 0.8)
 all_sheets_gar2 <- subset(all_sheets_gar1, all_sheets_gar1$TotalArea > 0.05)
 #all_sheets_gar3 <- subset(all_sheets_gar2, all_sheets_gar2$TotalArea > 0.02)
-hist(all_sheets_gar2$RadiusAverageRMS, 
-     main = paste(""),
-     xlab = "Средний радиус колонии, мм", 
-     ylab = "Частота встречаемости",
-     breaks = 100)
+# Skip filtered colonies histogram for now
+# hist(all_sheets_gar2$RadiusAverageRMS, ...)
 
-descdist(all_sheets_gar2$RadiusAverageRMS,  boot = 1000)
+# Skip filtered Cullen and Frey plot
+# descdist(all_sheets_gar2$RadiusAverageRMS,  boot = 1000)
 #data according to the  cullen and frey graph is lognormal distributed
 fit_lognorm <- fitdist(all_sheets_gar2$RadiusAverageRMS, "lnorm")
 
-plot(fit_lognorm)
+# Skip filtered diagnostic plot
+# plot(fit_lognorm)
 fit_lognorm
-ggplot() +
-  geom_histogram(aes(all_sheets_gar2$RadiusAverageRMS, y = ..density..), binwidth = .005) +
-  stat_function(fun = function(x) dlnorm(x, meanlog=fit_lognorm[[1]][1], sdlog=fit_lognorm[[1]][2]), color = "red",
-                size = 1) +
-  ylab("Частота встречаемости")+
-  xlab("Средний радиус колонии, мм")
+# Skip filtered histogram with fit
+# ggplot() + ...
 
 
 
@@ -168,31 +158,7 @@ ProbOfMonoclonality <- mean(replicate (10000, {
 }))
 ProbOfMonoclonality
 
-#with plot
-ProbOfMonoclonality <- mean(replicate (10000, {
-  #number of colonies in well
-  N <- 34
-  # (x,y) coordinate of centre. In square with area pi*34.8
-  x <- runif (N, 0, 31)
-  y <- runif (N, 0, 31)
-  #Beta distribution  with alpha = 9.849032, beta = 99.940056
-  r <- rlnorm(N, meanlog = fit_lognorm[[1]][1], sdlog = fit_lognorm[[1]][2])
-  C <- cbind(x,y,r)
-  #function which check if two random colonies overlap in matrix N x 3
-  func.c2 <- function(i,j) {
-    (C[i,1]-C[j,1])^2+(C[i,2]-C[j,2])^2 <= (C[i,3]+C[j,3])^2
-  }
-  #dp - matrix to fill with results of tested colonies
-  dp <- matrix(ncol = N, nrow = N)
-  for (i in c(1:N)) {
-    dp[i,] <- func.c2(i, )
-  }
-  sum((colSums(dp)-1) == 0)/N
-  plot(1:31,type="n", asp = 1)
-  index <- c(1:N)
-  for (i in index) {
-    draw.circle(x[i], y[i], r[i])
-  }
-}
-))
+# Skip final monte carlo simulation plot
+# png('results/final_monte_carlo_simulation.png')
+# ...
 
